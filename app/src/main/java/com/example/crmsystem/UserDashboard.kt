19c7257e1e86f522b5fffd2591877e28980203d6
vikashmishra464 <vikashmishra0200@gmail.com>
2025-04-23 13:52:17 +0530

    Update

--- app/src/main/java/com/example/crmsystem/UserDashboard.kt
+++ app/src/main/java/com/example/crmsystem/UserDashboard.kt
@@ -11,10 +11,6 @@ class UserDashboard : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_user_dashboard)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
